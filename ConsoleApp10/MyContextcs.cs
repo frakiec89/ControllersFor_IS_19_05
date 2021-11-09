937b8be46471711f465ac862ea165f02fce63659
frakiec89 <45938852+frakiec89@users.noreply.github.com>
2021-11-09 09:11:34 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp10/MyContextcs.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    public class PhoneContext : DbContext
+    {
+        public PhoneContext() 
+        { }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Phone> Phones { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=192.168.10.134;Database=zzzzz;User Id=Sa;Password=Frakiec89"); 
+        }
+
+    }
+
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (PhoneContext context = new PhoneContext())
+            {
+                SqlParameter param = new SqlParameter("@price", 58);
+
+                var phones = context.Phones.FromSqlRaw<Phone>("SELECT * FROM GetPhonesByPrice(@price)", param).
+                    Include(x=>x.Company).ToList();
+               
+                foreach (var u in phones)
+                    Console.WriteLine($" ID {u.Id} {u.Name} -{u.Price} -  {u.Company.Name} ");
+            }
+        }
+    }
+}
